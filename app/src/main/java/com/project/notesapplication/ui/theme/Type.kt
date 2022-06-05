cdcc2662bb0988e3e7c392157cc3ecaa127576e2
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-05 18:13:29 +0530

    The initial commit

--- app/src/main/java/com/project/notesapplication/ui/theme/Type.kt
+++ app/src/main/java/com/project/notesapplication/ui/theme/Type.kt
@@ -2,27 +2,67 @@ package com.project.notesapplication.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.project.notesapplication.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+private val fontFamilyKulim = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.kulim_park_regular
+        ),
+        Font(
+            resId = R.font.kulim_park_light,
+            weight = FontWeight.Light
+        )
     )
-    /* Other default text styles to override
+
+)
+private val fontFamilyLato = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.lato_regular
+        ),
+        Font(
+            resId = R.font.lato_bold,
+            weight = FontWeight.Bold
+        )
+    )
+)
+
+
+val Typography = Typography(
+    defaultFontFamily = fontFamilyLato,
+    h1 = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontWeight = FontWeight.Light,
+        fontSize = 28.sp,
+        letterSpacing = (1.15).sp
+    ),
+    h2 = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontSize = 15.sp,
+        letterSpacing = (1.15).sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    body1 = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = (1.15).sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontFamily = fontFamilyKulim,
+        fontSize = 12.sp,
+        letterSpacing = (1.15).sp
+    ),
 )
