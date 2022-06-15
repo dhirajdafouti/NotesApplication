fefb671138ad5309cf0558a71f9b2472aa477863
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-15 22:34:37 +0530

    The initial commit, basic structure.

--- app/src/main/java/com/project/notesapplication/feature_note/MainActivity.kt
+++ app/src/main/java/com/project/notesapplication/feature_note/MainActivity.kt
@@ -3,14 +3,29 @@ package com.project.notesapplication.feature_note
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.project.notesapplication.ui.theme.NotesApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesApplicationTheme {
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Green)) {
+                    Column {
+                        Text(text = "Welcome to Notes Application...")
+                    }
 
+                }
             }
         }
     }
