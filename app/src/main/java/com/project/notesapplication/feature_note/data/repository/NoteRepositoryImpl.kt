24d43ad4ae1ee44063926a09eaa7a6a14e8f50be
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-25 18:27:41 +0530

    The repository interface and repository impl for the application is completed

---
+++ app/src/main/java/com/project/notesapplication/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.project.notesapplication.feature_note.data.repository
+
+import com.project.notesapplication.feature_note.data.data_source.NoteDao
+import com.project.notesapplication.feature_note.domain.model.Note
+import com.project.notesapplication.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl(private val dao: NoteDao) : NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return dao.getNoteById(id)
+    }
+
+    override suspend fun insertNote(note: Note) {
+        return dao.insertNote(note)
+    }
+
+    override suspend fun deleteNotes(note: Note) {
+        return dao.deleteNote(note)
+    }
+}
---
+++ app/src/main/java/com/project/notesapplication/feature_note/domain/repository/NoteRepository.kt
@@ -0,0 +1,14 @@
+package com.project.notesapplication.feature_note.domain.repository
+
+import com.project.notesapplication.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+interface NoteRepository {
+    fun getNotes(): Flow<List<Note>>
+
+    suspend fun getNoteById(id: Int): Note?
+
+    suspend fun insertNote(note: Note)
+
+    suspend fun deleteNotes(note: Note)
+}
