29dec5a2ae9c2f035c09877ae1426ad818ba71db
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-07-04 21:58:45 +0530

    The notes components

--- app/src/main/java/com/project/notesapplication/feature_note/presentation/notes/notesui/NotesScreen.kt
+++ app/src/main/java/com/project/notesapplication/feature_note/presentation/notes/notesui/NotesScreen.kt
@@ -1,13 +1,21 @@
 package com.project.notesapplication.feature_note.presentation.notes.notesui
 
+import androidx.compose.animation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Sort
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.project.notesapplication.feature_note.presentation.notes.NotesEvent
 import com.project.notesapplication.feature_note.presentation.notes.NotesViewModel
+import com.project.notesapplication.feature_note.presentation.notes.components.OrderSection
 
 @Composable
 fun NotesScreen(
@@ -21,9 +29,43 @@ fun NotesScreen(
         FloatingActionButton(onClick = {
 
         }, backgroundColor = MaterialTheme.colors.primary) {
-            Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add Note")
         }
     }, scaffoldState = scaffoldState) {
+
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "Your Notes",
+                    style = MaterialTheme.typography.h4
+                )
+                IconButton(onClick = {
+                    viewModel.onEvent(NotesEvent.ToggleOrderSection)
+
+                }) {
+                    Icon(imageVector = Icons.Default.Sort,
+                        contentDescription = "Sort")
+
+                }
+            }
+            AnimatedVisibility(visible = state.isOrderSectionVisible,
+                enter = fadeIn() + slideInVertically(),
+                exit = fadeOut() + slideOutVertically()) {
+                OrderSection(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                    noteOrder = state.notesOrder,
+                    onOrderChange = {
+                        viewModel.onEvent(NotesEvent.Order(it))
+                    })
+
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
 
     }
 
